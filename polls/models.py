21a0d77f7aa3d030ffc9bbb76c0c6977f7935e3b
Guillaume <guillaume@Guillaumes-iMac.local>
2017-08-22 11:38:38 +0200

    partie 5

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,6 @@ import datetime
 # from __future__ import unicode_literals
 
 from django.db import models
-
 from django.utils import timezone
 
 
@@ -16,7 +15,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
